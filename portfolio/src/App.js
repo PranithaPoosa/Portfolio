ef458d8af26f0db74f8664aac2299ad4b3e3a025
Mani Deepak Reddy Aila <manideepakreddyaila@Manis-MacBook-Air.local>
2023-01-21 16:27:20 -0500

    added basic code

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {Link, BrowserRouter, Routes, Route} from "react-router-dom";
+import React from "react";
+import Projects  from "./components/Projects";
+import About from "./components/About";
+import Contributors from "./components/Contributors";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route path="/projects" element={<Projects/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contributors" element={<Contributors/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
