4b04cdd546b828265847b96eabd5567e3cf557a1
Mani Deepak Reddy Aila <manideepakreddyaila@Manis-MacBook-Air.local>
2023-01-22 00:21:47 -0500

    changes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,13 +4,16 @@ import Projects  from "./components/Projects";
 import About from "./components/About";
 import Contributors from "./components/Contributors";
 import NavBar from "./components/NavBar";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <NavBar/>
+        
         <Routes>
+          <Route path="/" element={<HomePage/>} />
           <Route path="/projects" element={<Projects/>} />
           <Route path="/about" element={<About/>} />
           <Route path="/contributors" element={<Contributors/>} />
