1ea855df90ca5d04ea4134a1c400e41ad09b5baa
Mani Deepak Reddy Aila <manideepakreddyaila@Manis-MacBook-Air.local>
2023-01-22 14:09:49 -0500

    added image and styling changes

--- portfolio/src/components/HomePage.js
+++ portfolio/src/components/HomePage.js
@@ -4,14 +4,19 @@ import {Link} from 'react-router-dom'
 function HomePage() {
   return (
     <div>
-        <div style={{ height: '-webkit-fill-available', width: '100%', overflow:'scroll', backgroundImage: 'url(/assets/bg2.jpg)'}}>
-            <h1>Hey, I'm Mani Deepak Reddy Aila</h1>
-            <h3>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</h3>
-            <Link to="/projects">
-                <button type="button">
-                    PROJECTS
-                </button>
-            </Link>
+        <div style={{ display: 'flex', justifyContent:'space-evenly',height: '-webkit-fill-available', width: '100%', backgroundColor: '#f9f9ff', alignItems: 'center'}}>
+            <div style={{padding: '8%'}}>
+                <h1>Hey, I'm Mani Deepak Reddy Aila</h1>
+                <h3>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</h3>
+                <Link to="/projects">
+                    <button type="button">
+                        PROJECTS
+                    </button>
+                </Link>
+            </div>
+            <div style={{padding: '10%'}}>
+                <img src='/assets/self.jpg'/>
+            </div>
         </div>
         <div>
             <h1>
