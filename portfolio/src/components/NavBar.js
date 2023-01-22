cf1f3b66c29f5d4fd1eb72a654d48213d6a0a504
PranithaPoosa <poosa.p@northeastern.edu>
2023-01-22 15:46:49 -0500

    navbar changes

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import {Link} from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
 
 function NavBar() {
   return (
     <div>
       <h3>NavBar</h3>
+      <Nav variant="tabs" defaultActiveKey="/home">
+      <Nav.Item>
+        <Nav.Link href='/HomePage'>Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link>About</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link>
+          Projects
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
     </div>
-  )
+  );
 }
 
 export default NavBar
