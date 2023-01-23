2dba1f6cadcef36170f743f311b02af4c02e1b94
Mani Deepak Reddy Aila <manideepakreddyaila@Manis-MacBook-Air.local>
2023-01-22 23:12:40 -0500

    changes to navbar

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -9,33 +9,33 @@ function Projects() {
           <p style={{textAlign:'center', padding:'2%', fontWeight:'bold'}}>PROJECTS</p>
         </div>
 
-       <div style={{paddingTop:'2%'}}>
-       <Card style={{ width: '50rem' , flexDirection:'row'}}>
-       <Card.Img src="/assets/image.png" class="card-img-top" style={{width:'30%'}}alt="project"/>
-        <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
-       </Card>
+       <div style={{paddingTop:'2%', textAlign: '-webkit-center'}}>
+        <Card style={{ width: '60rem' , flexDirection:'row', margin: '3% 0%'}}>
+        <Card.Img src="/assets/image.png" class="card-img-top" style={{width:'30%'}}alt="project"/>
+          <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
 
-       <Card style={{ width: '50rem' , flexDirection:'row', borderBlockColor:'none'}}>
-       <Card.Img src="/assets/image.png" class="card-img-top" style={{width:'30%'}}alt="project"/>
-        <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
-       </Card>
-    </div>
+        <Card style={{ width: '60rem' , flexDirection:'row', borderBlockColor:'none', margin: '3% 0%'}}>
+        <Card.Img src="/assets/image.png" class="card-img-top" style={{width:'30%'}}alt="project"/>
+          <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 }
