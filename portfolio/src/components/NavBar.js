6b77cd8b60743cecc94b89186b02da74572c2007
Mani Deepak Reddy Aila <manideepakreddyaila@Manis-MacBook-Air.local>
2023-01-24 12:02:34 -0500

    ui modifications

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -4,21 +4,21 @@ import Nav from 'react-bootstrap/Nav';
 
 function NavBar() {
   return (
-      <Nav variant="pills" style={{backgroundColor: 'white', position:'sticky', top: '0', width: '100%', display:'flex', justifyContent: 'space-between', padding: '1% 5%'}}>
+      <Nav variant="pills" style={{backgroundColor: '#3AAFA9', position:'sticky', top: '0', width: '100%', display:'flex', justifyContent: 'space-between', padding: '1% 5%'}}>
         <div style = {{paddingLeft: '5%'}}>
           <Nav.Item>
-            <Nav.Link style={{color:'black'}} href="/"><span><b>A. MANI DEEPAK</b></span></Nav.Link>
+            <Nav.Link style={{backgroundColor: '#3AAFA9', color: 'white', fontSize: '100%'}} href="/"><span><b>A. MANI DEEPAK</b></span></Nav.Link>
           </Nav.Item>
         </div>
         <div style={{display: 'flex'}}>
           <Nav.Item>
-            <Nav.Link style={{color:'black'}} href="/">Home</Nav.Link>
+            <Nav.Link style={{backgroundColor: '#000000', color: '#FEFFFF'}} href="/">Home</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link style={{color:'black'}} href="/projects">Projects</Nav.Link>
+            <Nav.Link style={{backgroundColor: '#000000', color: '#FEFFFF'}} href="/projects">Projects</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link style={{color:'black'}} href="/about">
+            <Nav.Link style={{backgroundColor: '#000000', color: '#FEFFFF'}} href="/about">
               About
             </Nav.Link>
           </Nav.Item>
