25c0aeed1a26f1df3e77011a6d5170505b531070
Mani Deepak Reddy Aila <manideepakreddyaila@Manis-MacBook-Air.local>
2023-01-24 01:06:20 -0500

    changes to navbar

--- portfolio/src/components/HomePage.js
+++ portfolio/src/components/HomePage.js
@@ -1,16 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
-import {
-    MDBCarousel,
-    MDBCarouselInner,
-    MDBCarouselItem,
-    MDBContainer,
-    MDBRow,
-    MDBCol,
-    MDBIcon,
-  } from "mdb-react-ui-kit";
-  import Carousel from 'react-bootstrap/Carousel';
+import Form from 'react-bootstrap/Form';
   
 function HomePage() {
   return (
@@ -83,128 +74,31 @@ function HomePage() {
             <h1>
                 Reviews
             </h1>
-            {/*
-            <MDBContainer className="my-5">
-                <MDBCarousel showControls dark>
-                    <MDBCarouselInner>
-                        <MDBCarouselItem className="active text-center">
-                            <img
-                            src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp"
-                            alt="avatar"
-                            className="rounded-circle shadow-1-strong mb-4"
-                            style={{ width: "150px" }}
-                            />
-                            <MDBRow className="d-flex justify-content-center">
-                            <MDBCol lg="8">
-                                <h5 className="mb-3">Maria Kate</h5>
-                                <p>Photographer</p>
-                                <p className="text-muted">
-                                <MDBIcon fas icon="quote-left" className="pe-2" />
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-                                et deleniti nesciunt sint eligendi reprehenderit reiciendis,
-                                quibusdam illo, beatae quia fugit consequatur laudantium velit
-                                magnam error. Consectetur distinctio fugit doloremque.
-                                </p>
-                            </MDBCol>
-                            </MDBRow>
-                            <ul className="list-unstyled d-flex justify-content-center text-warning mb-0">
-                            <li>
-                                <MDBIcon fas icon="star" size="sm" />
-                            </li>
-                            <li>
-                                <MDBIcon fas icon="star" size="sm" />
-                            </li>
-                            <li>
-                                <MDBIcon fas icon="star" size="sm" />
-                            </li>
-                            <li>
-                                <MDBIcon fas icon="star" size="sm" />
-                            </li>
-                            <li>
-                                <MDBIcon far icon="star" size="sm" />
-                            </li>
-                            </ul>
-                        </MDBCarouselItem>
+                 
+        </div>
+        <div style={{padding: '4% 0% 10% 0%', textAlign: 'center', backgroundColor: '#ffffff'}}>
+            <h1>
+                Contact
+            </h1>
+            <br/>
+            <Form style={{margin: '0% 20%', backgroundColor: '#ebebeb', padding: '2%', borderRadius: '6px'}}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type='text' placeholder="Enter name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
 
-                        <MDBCarouselItem className="text-center">
-                            <img
-                            src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp"
-                            alt="avatar"
-                            className="rounded-circle shadow-1-strong mb-4"
-                            style={{ width: "150px" }}
-                            />
-                            <MDBRow className="d-flex justify-content-center">
-                            <MDBCol lg="8">
-                                <h5 className="mb-3">John Doe</h5>
-                                <p>Web Developer</p>
-                                <p className="text-muted">
-                                <MDBIcon fas icon="quote-left" className="pe-2" />
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-                                et deleniti nesciunt sint eligendi reprehenderit reiciendis.
-                                </p>
-                            </MDBCol>
-                            </MDBRow>
-                            <ul className="list-unstyled d-flex justify-content-center text-warning mb-0">
-                            <li>
-                                <MDBIcon fas icon="star" size="sm" />
-                            </li>
-                            <li>
-                                <MDBIcon fas icon="star" size="sm" />
-                            </li>
-                            <li>
-                                <MDBIcon fas icon="star" size="sm" />
-                            </li>
-                            <li>
-                                <MDBIcon fas icon="star" size="sm" />
-                            </li>
-                            <li>
-                                <MDBIcon far icon="star" size="sm" />
-                            </li>
-                            </ul>
-                        </MDBCarouselItem>
-
-                        <MDBCarouselItem className="text-center">
-                            <img
-                            src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp"
-                            alt="avatar"
-                            className="rounded-circle shadow-1-strong mb-4"
-                            style={{ width: "150px" }}
-                            />
-                            <MDBRow className="d-flex justify-content-center">
-                            <MDBCol lg="8">
-                                <h5 className="mb-3">Anna Deynah</h5>
-                                <p>Web Developer</p>
-                                <p className="text-muted">
-                                <MDBIcon fas icon="quote-left" className="pe-2" />
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-                                et deleniti nesciunt sint eligendi reprehenderit reiciendis,
-                                quibusdam illo, beatae quia fugit consequatur laudantium velit
-                                magnam error. Consectetur distinctio fugit doloremque.
-                                </p>
-                            </MDBCol>
-                            </MDBRow>
-                            <ul className="list-unstyled d-flex justify-content-center text-warning mb-0">
-                            <li>
-                                <MDBIcon fas icon="star" size="sm" />
-                            </li>
-                            <li>
-                                <MDBIcon fas icon="star" size="sm" />
-                            </li>
-                            <li>
-                                <MDBIcon fas icon="star" size="sm" />
-                            </li>
-                            <li>
-                                <MDBIcon fas icon="star" size="sm" />
-                            </li>
-                            <li>
-                                <MDBIcon far icon="star" size="sm" />
-                            </li>
-                            </ul>
-                        </MDBCarouselItem>
-                    </MDBCarouselInner>
-                </MDBCarousel>
-                </MDBContainer>
-                */}     
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control as="textarea" placeholder="Enter message" rows={6} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+                </Form> 
         </div>
     </>
   )
